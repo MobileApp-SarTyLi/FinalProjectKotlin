aefa31dd93ece7da3d81388cc26cd0e53828c562
Tyler Liu <42123666+TylerLiu88@users.noreply.github.com>
2021-10-24 23:28:54 -0400

    Added Creation of Data

--- app/src/main/java/com/example/mobapps/MainActivity.kt
+++ app/src/main/java/com/example/mobapps/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.mobapps
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
@@ -8,29 +9,58 @@ import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import com.example.mobapps.ui.main.SectionsPagerAdapter
 import com.example.mobapps.databinding.ActivityMainBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var database: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        Log.d(null, "onCreate() for Second Fragment")
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
-        val viewPager: ViewPager = binding.viewPager
-        viewPager.adapter = sectionsPagerAdapter
-        val tabs: TabLayout = binding.tabs
-        tabs.setupWithViewPager(viewPager)
-        val fab: FloatingActionButton = binding.fab
+        //TODO: Change this to an individual fragment
+        //Login/Register Button
+        binding.registerBtn.setOnClickListener {
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            val firstName = binding.firstName.text.toString()
+            val lastName = binding.lastName.text.toString()
+            val age = binding.age.text.toString()
+            val userName = binding.userName.text.toString()
+
+            database = FirebaseDatabase.getInstance().getReference("User")
+            val User = User(firstName, lastName, age, userName)
+            database.child(userName).setValue(User).addOnSuccessListener {
+
+                binding.firstName.text.clear()
+                binding.lastName.text.clear()
+                binding.age.text.clear()
+                binding.userName.text.clear()
+
+                Toast.makeText(this, "Success!", Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener{
+                Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show()
+            }
+
         }
+
+//        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+//        val viewPager: ViewPager = binding.viewPager
+//        viewPager.adapter = sectionsPagerAdapter
+//        val tabs: TabLayout = binding.tabs
+//        tabs.setupWithViewPager(viewPager)
+//        val fab: FloatingActionButton = binding.fab
+//
+//        fab.setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                    .setAction("Action", null).show()
+//        }
     }
 }
---
+++ app/src/main/java/com/example/mobapps/User.kt
@@ -0,0 +1,5 @@
+package com.example.mobapps
+
+data class User(val firstName : String? = null,val lastName : String? = null,val age : String? = null,val userName : String? = null){
+
+}
