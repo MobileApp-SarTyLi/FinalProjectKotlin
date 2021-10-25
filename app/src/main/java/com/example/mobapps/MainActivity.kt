22255eef0eb8255fc8ba0350b91994ecbb448c73
Tyler Liu <42123666+TylerLiu88@users.noreply.github.com>
2021-10-25 01:16:58 -0400

    Added Data Retreival

--- app/src/main/java/com/example/mobapps/MainActivity.kt
+++ app/src/main/java/com/example/mobapps/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mobapps
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -9,11 +10,13 @@ import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
 import android.widget.Toast
 import com.example.mobapps.ui.main.SectionsPagerAdapter
 import com.example.mobapps.databinding.ActivityMainBinding
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,7 +31,8 @@ class MainActivity : AppCompatActivity() {
 
         //TODO: Change this to an individual fragment
         //Login/Register Button
-        binding.registerBtn.setOnClickListener {
+        val button = findViewById<Button>(R.id.registerBtn)
+        button.setOnClickListener {
 
             val firstName = binding.firstName.text.toString()
             val lastName = binding.lastName.text.toString()
@@ -45,11 +49,70 @@ class MainActivity : AppCompatActivity() {
                 binding.userName.text.clear()
 
                 Toast.makeText(this, "Success!", Toast.LENGTH_SHORT).show()
-            }.addOnFailureListener{
+            }.addOnFailureListener {
                 Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show()
             }
 
         }
+
+        val button2 = findViewById<Button>(R.id.nextPageBtn)
+        button2.setOnClickListener{
+            val intent = Intent(this, ReadData::class.java)
+            startActivity(intent)
+        }
+
+        binding.readdataBtn.setOnClickListener {
+
+            val userName : String = binding.etusername.text.toString()
+            if  (userName.isNotEmpty()){
+
+                readData(userName)
+
+            }else{
+
+                Toast.makeText(this,"PLease enter Username",Toast.LENGTH_SHORT).show()
+
+            }
+
+        }
+
+    }
+
+    private fun readData(userName: String) {
+
+        database = FirebaseDatabase.getInstance().getReference("User")
+        database.child(userName).get().addOnSuccessListener {
+
+            if (it.exists()){
+
+                val firstname = it.child("firstName").value
+                val lastName = it.child("lastName").value
+                val age = it.child("age").value
+                Toast.makeText(this,"Successfuly Read",Toast.LENGTH_SHORT).show()
+                binding.etusername.text.clear()
+                binding.tvFirstName.text = firstname.toString()
+                binding.tvLastName.text = lastName.toString()
+                binding.tvAge.text = age.toString()
+
+            }else{
+
+                Toast.makeText(this,"User Doesn't Exist",Toast.LENGTH_SHORT).show()
+
+
+            }
+
+        }.addOnFailureListener{
+
+            Toast.makeText(this,"Failed",Toast.LENGTH_SHORT).show()
+
+
+        }
+
+
+
+    }
+
+
 
 //        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
 //        val viewPager: ViewPager = binding.viewPager
@@ -62,5 +125,5 @@ class MainActivity : AppCompatActivity() {
 //            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                    .setAction("Action", null).show()
 //        }
-    }
+
 }
